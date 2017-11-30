fda46007ad0eb5ba49b1d054b2817bb57beffc60
Hernán Torres <hernan.torres@ucr.ac.cr>
2017-11-29 18:23:43 -0600

    .

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -6,20 +6,21 @@ import javax.swing.JTextField;
 import java.util.Scanner;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-
 import javax.swing.JPanel;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.Insets;
 import java.awt.Toolkit;
-
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import javax.swing.Timer;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 /**
  * Class that creates the main wondow for the game. Extends JFrame.
  * This object displays the image and text field where the game takes place.
  */
-public class MainWindow extends JFrame
+public class MainWindow extends JFrame implements ActionListener
 {
 	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 	Insets scnMax = Toolkit.getDefaultToolkit().getScreenInsets( getGraphicsConfiguration() );
@@ -36,6 +37,11 @@ public class MainWindow extends JFrame
 	private static int textLength = 30;
 	private static String gameName = "Finite Sentence Machine";
 
+	private Timer elapsedTime = null;
+   	private long elapsedSeconds = 0;
+
+   	private JLabel timerView;
+
 	/** Constructor */
 	public MainWindow()
 	{
@@ -46,35 +52,14 @@ public class MainWindow extends JFrame
 		BorderLayout mainLayout = new BorderLayout();
 		this.setLayout(mainLayout);
 		displayElements();
+		this.elapsedTime = new Timer(1000, this);
+   		this.elapsedTime.start();
+
+
 	}
 	
  
 
-	/**
-	* 
-	*/
-	/*private void displayImageView()
-	{
-
-		System.out.printf("monitor height = %.2f %n", monitorHeight);
-		System.out.printf("monitor width = %.2f %n", monitorWidth);
-		System.out.printf("window height = %d %n", windowHeight);
-		System.out.printf("window width = %d %n", windowWidth);
-		
-		ImageIcon image = new ImageIcon("../images/imgJoseFigueres.png");
-
-		JLabel character = new JLabel("Adivina el personaje:", image, JLabel.CENTER);
-
-		// Coloca el Label arriba
-		character.setVerticalAlignment(JLabel.TOP);
-
-		// Coloca el texto encima de la imagen
-		character.setVerticalTextPosition(JLabel.TOP);
-		character.setHorizontalTextPosition(JLabel.CENTER);
-
-		// Añade el Label al Frame
-		this.add(character);
-	}*/
 
    /** 
     *
@@ -83,32 +68,50 @@ public class MainWindow extends JFrame
 	{
 		ImageIcon image = new ImageIcon( "../assets/images/louisCK.png");
 		JLabel imageView = new JLabel(image);
-		// String imagePathName = "../assets/images/louisCK.png";
-		// ImagePanel imagePanel = new ImagePanel( imagePathName );
-		JLabel timerView = new JLabel("00:01", JLabel.CENTER);
+		this.timerView = new JLabel();
 		
 
-		// JPanel levelIndicator = new JPanel();
-		// levelIndicator.setLayout(new BorderLayout());
 		JLabel currentLevelView = new JLabel("LEVEL 1");
 		Font font = new Font("Sans", Font.ITALIC, 20);
 		currentLevelView.setFont( font );
 		currentLevelView.setHorizontalAlignment(JLabel.CENTER);
-		// levelIndicator.add(level);
+
 
 		JTextField text = new JTextField(10);
 		text.setFont(font);
 		JPanel entryPanel = new JPanel();
 		entryPanel.add(text);
 		JPanel southPanel = new JPanel( new GridLayout(2,1));
+		timerView.setHorizontalAlignment(JLabel.CENTER);
+		timerView.setFont ( font );
 		southPanel.add(timerView);
 		southPanel.add(entryPanel);
 
 		// Añade los elementos al Frame
 		this.add(currentLevelView, BorderLayout.NORTH);
-		// this.add(imagePanel);
 		this.add(imageView, BorderLayout.CENTER);
 		this.add(southPanel, BorderLayout.SOUTH);
 
 	}
+
+   @Override
+   public void actionPerformed(ActionEvent event)
+   {
+      if ( event.getSource() == this.elapsedTime )
+      {
+         this.updateElapsedTime();
+      }
+   }
+ 
+   private void updateElapsedTime()
+   {
+      ++this.elapsedSeconds;
+      long minutes = this.elapsedSeconds / 60;
+      long seconds = this.elapsedSeconds % 60;
+      
+      String text = String.format("%02d:%02d"
+            , minutes
+            , seconds);
+      this.timerView.setText(text);
+   }
 }
